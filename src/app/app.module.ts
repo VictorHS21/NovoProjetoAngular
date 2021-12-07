3c1f97ac899947020ea857477954c68c563c028e
Victor Hugo Schmidt <victor.s@lyncas.net>
2021-12-07 18:38:19 -0300

    Ajuste para valor monetario do Brasil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './navegacao/home/home.component';
 import { FooterComponent } from './navegacao/footer/footer.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
+
 import { RouterModule } from '@angular/router';
 import { rootRouterConfig } from './app.routes';
 import { APP_BASE_HREF } from '@angular/common';
@@ -15,6 +16,10 @@ import { FormsModule } from '@angular/forms';
 import { ProdutoService } from './produtos/produtos.service';
 import { ListarProdutoComponent } from './produtos/listar-produto/listar-produto.component';
 import { HttpClientModule } from '@angular/common/http';
+
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
